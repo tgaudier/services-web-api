8785097ae4220c1f48b2520fbb046a224312c975
Tibo <thibaut.gaudier78@gmail.com>
2020-05-13 15:21:28 +0200

    init

---
+++ server.js
@@ -0,0 +1,9 @@
+const PORT = 8080
+
+const app = require('express')()
+const http = require('http').Server(app)
+
+app.use('/', require("./api.js"))
+
+http.listen(PORT)
+console.log("Server started on port " + PORT)
